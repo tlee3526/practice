bbd44ccbcf3395171dbf5a176d789844e54cb8a8
BT_1N3_21@BKTECH.LOCAL <BT_1N3_21@BKTECH.LOCAL>
2018-09-26 09:24:15 -0400

    Initial commit

---
+++ src/hi.java
@@ -0,0 +1,17 @@
+public class hi {
+    public static void main (String[] augs)
+    {
+        String str1 = "LoveJava";
+        while (str1.length() - 1 > 0)
+        {
+            System.out.println(str1);
+            str1 = str1.substring(1, str1.length() - 1);
+        }
+        String str2 = "What?";
+        for (int i = 0; i < str2.length(); i ++)
+        {
+            System.out.println(str2);
+            str2 = str2.substring(str2.length()- 1) + str2.substring(0, str2.length()-1);
+        }
+    }
+}
